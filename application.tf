21c54ff0b453762f7e8c7b90646c22e636caa960
Conor Murphy <conmurph@cisco.com>
2021-02-11 23:03:49 +0100

    initial

--- application.tf
+++ application.tf
@@ -3,16 +3,24 @@ data "aci_tenant" "conmurph" {
   name  = "conmurph"
 }
 
+resource "aci_application_profile" "application_1" {
+  tenant_dn  = "${data.aci_tenant.conmurph.id}"
+  name       = "application_1"
+}
+
+resource "aci_application_epg" "application_1" {
+    application_profile_dn  = "${aci_application_profile.application_1.id}"
+    name                              = "application_1"
+}
 
 resource "aci_bridge_domain" "application_1" {
-  tenant_dn   = "${aci_tenant.conmurph.id}"
+  tenant_dn   = "${data.aci_tenant.conmurph.id}"
   name        = "application_1"
   description = "This bridge domain is created by terraform ACI provider"
 }
 
 resource "aci_subnet" "application_1" {
-  bridge_domain_dn                    = "${aci_bridge_domain.application_1.id}"
-  ip                                  = "10.0.3.28/27"
-  scope                               = "private"
+  parent_dn                    = "${aci_bridge_domain.application_1.id}"
+  ip                                  = "1.1.1.254/24"
   description                         = "This subject is created by terraform"
 }
