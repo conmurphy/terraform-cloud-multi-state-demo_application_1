5c9762c628f0ce16350468db49f6bc5f7cb78960
Conor Murphy <conmurph@cisco.com>
2021-02-11 23:19:48 +0100

    Creating Application 1 EPGs in the same tenant:conmurph

--- application.tf
+++ application.tf
@@ -3,7 +3,7 @@ data "aci_tenant" "conmurph" {
   name  = "conmurph"
 }
 
-/*
+
 resource "aci_application_profile" "application_1" {
   tenant_dn  = "${data.aci_tenant.conmurph.id}"
   name       = "application_1"
@@ -25,4 +25,4 @@ resource "aci_subnet" "application_1" {
   parent_dn                    = "${aci_bridge_domain.application_1.id}"
   ip                                  = "1.1.1.243/24"
   description                         = "This subject is created by terraform"
-}*/
+}
